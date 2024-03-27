db1f54968b856044e9797787d39b78030c2e15cd
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2024-03-27 12:26:32 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+document.getElementById('round1').innerHTML = Math.floor(Math.random() * 100);
+document.getElementById('round2').innerHTML = Math.floor(Math.random() * 100);
+
+var index = 0;
+var selectedOperation = '+';
+
+function refresh() {
+    document.getElementById('round1').innerHTML = Math.floor(Math.random() * 100);
+    document.getElementById('round2').innerHTML = Math.floor(Math.random() * 100);
+
+    document.getElementsByTagName("body")[0].style.background = numbers[index++];
+
+    if (index > numbers.length - 1)
+        index = 0;
+}
+
+function selectOperation(operation) {
+    selectedOperation = operation;
+    document.getElementById('selectedOperation').innerText = operation;
+}
+
+function check(){
+    var round1 = parseInt(document.getElementById('round1').innerHTML);
+    var round2 = parseInt(document.getElementById('round2').innerHTML);
+    var userGuess = parseInt(document.getElementById('userAnswer').value);
+    
+    var result;
+    switch(selectedOperation) {
+        case '+':
+            result = round1 + round2;
+            break;
+        case '-':
+            result = round1 - round2;
+            break;
+        case '*':
+            result = round1 * round2;
+            break;
+        case '/':
+            result = round1 / round2;
+            break;
+        case '%':
+            result = round1 % round2;
+            break;
+    }
+    
+    if(userGuess === result) {
+        alert("Congratulations!");
+    } else {
+        alert("Incorrect! The correct answer is: " + result);
+    }
+}
